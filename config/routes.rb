0f05699ee8a6e066c4afaa7d422459f09f9eb76d
Dan Wiseman <daniel.wiseman@gmail.com>
2020-08-21 11:56:53 +0000

    began working on the master controller and finished setting up rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  
+  scope '/decks' do
+    resources :master_decks
+  end
+    
 end
