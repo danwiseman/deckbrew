d2f722ccb486e57877ed64955d85d62355d8ff2e
Dan Wiseman <daniel.wiseman@gmail.com>
2020-08-25 11:48:44 +0000

    some fixes to the branch tests, as it actually doesn't work, but the branch page does

---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+    
+    it "shows the user's profile screen after log in" do
+    
+       user = FactoryBot.create(:user)
+       
+       visit "/accounts/sign_in"
+       fill_in "user_login", :with => user.username
+       fill_in "user_password", :with => user.password
+       click_button "Log in"
+       
+       expect(page).to have_current_path('/u/#{user.slug}')
+       
+    end
+
+end
