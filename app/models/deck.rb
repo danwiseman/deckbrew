90a59bcdebac65c5423293b9853e13d8b160a746
Dan Wiseman <daniel.wiseman@gmail.com>
2020-08-31 12:33:17 +0000

    added deck model validation

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,7 @@
 class Deck < ApplicationRecord
     belongs_to :branch
     
+    validates_presence_of :version
+    validates_presence_of :previousversion
     
 end
--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Deck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+    before(:each) do
+        @br = FactoryBot.create(:branch)
+    end
+      
+    it "is valid with valid attributes" do
+        d = Deck.new(previousversion: 1, version: 3, branch: @br)
+        expect(d).to be_valid
+    end
+    
+    it "is not valid without a branch" do
+        d = Deck.new(branch: nil, previousversion: 0, version: 0)
+        expect(d).to_not be_valid
+    end
+    
+    it "is not valid without a version" do 
+        d = Deck.new(version: nil, previousversion: 3, branch: @br)
+        expect(d).to_not be_valid
+    end
+    
+    it "is not valid without a previousversion" do
+        d = Deck.new(previousversion: nil, version: 3, branch: @br)
+        expect(d).to_not be_valid
+    end
+    
+    
 end
