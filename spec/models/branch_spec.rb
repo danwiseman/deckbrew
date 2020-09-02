11e8a699a4116fbbf11bc694e57ef1a352688cf0
Dan Wiseman <daniel.wiseman@gmail.com>
2020-09-02 11:40:42 +0000

    added some details to dbseed. fixed an error with branch model spec

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -21,8 +21,9 @@ RSpec.describe Branch, type: :model do
     end
     
     it "has a source_branch accessor for easy management of branching" do
-        br = Branch.new(name: "new branch", master_deck: nil, branched_from: { source_branch: 0, source_deck: 0 } )
+        br = Branch.new(name: "new branch", master_deck: @md, branched_from: { source_branch: 0, source_deck: 0 } )
         expect(br.source_branch).to eq(0)
+        expect(br).to be_valid
     end
         
 
