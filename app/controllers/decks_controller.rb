fb89e97f702771fe2be55bad996a6068bb552c24
Dan Wiseman <daniel.wiseman@gmail.com>
2020-09-07 14:29:53 +0000

    began work on the card adding form

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,6 +1,8 @@
 class DecksController < ApplicationController
     
     before_action :authenticate_user!, only: [:edit]
+    before_action :set_master_deck_and_current_branch, only: [:edit]
+    
 
     def edit
         
@@ -9,6 +11,12 @@ class DecksController < ApplicationController
     end
     
 
-
+    private
+    
+    def set_master_deck_and_current_branch
+        @master_deck = current_user.master_decks.friendly.find(params[:master_deck_id])
+        @branch = @master_deck.branches.friendly.find(params[:branch_id])
+        
+    end
     
 end
---
+++ spec/requests/cards_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Cards", type: :request do
+    
+    
+    it "grabs a card from scryfall"
+    it "displays a card"
+    
+    
+    
+end
