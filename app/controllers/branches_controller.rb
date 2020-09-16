9d64e51cba785708b7fb79227c00f26113d05a0d
Daniel Wiseman <daniel.wiseman@gmail.com>
2020-09-16 22:23:27 +0000

    removed some puts from the controller for troubleshooting

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -80,7 +80,7 @@ class BranchesController < ApplicationController
     end
     
     def merge
-        puts params
+    
        source_b_id = params['source_branch']['id']
        dest_b_id = params['destination_branch']['destination_branch_id']
        
