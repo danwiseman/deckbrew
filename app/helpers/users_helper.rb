5150896d3e4ea9116c7a9863f4a50dc01b64f83e
Daniel Wiseman <daniel.wiseman@gmail.com>
2020-09-18 11:40:27 +0000

    added gravatar as the user's avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
         '/u/' + user.slug
     end
     
+        
+    def user_gravatar(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+        image_tag(gravatar_url, alt: user.username)
+    end
+
+    
 end
