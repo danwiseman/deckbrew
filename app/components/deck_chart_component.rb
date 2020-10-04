63d3d94f5541f847342fb46a56cb88b5a23096ab
Daniel Wiseman <daniel.wiseman@gmail.com>
2020-10-04 13:00:42 +0000

    worked on creating a separate view for the charts

---
+++ app/components/deck_chart_component.rb
@@ -0,0 +1,31 @@
+class DeckChartComponent < ViewComponent::Base
+    
+    def initialize(deck:, chart_type:)
+        @deck = deck
+        @chart_type = chart_type
+
+    end
+    
+    def mana_curve_data(deck)
+        
+    end
+    
+    def mana_curve_labels(deck)
+        
+    end
+    
+    def card_type_data(deck)
+        
+    end
+    
+    def card_type_labels(deck)
+    
+    end
+    
+    def cmc_data(deck)
+        
+    end
+    
+    
+    
+end
